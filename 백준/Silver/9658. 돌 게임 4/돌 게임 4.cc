fadb4b975722f2b079c50cd7f9d6d2c720385c66
min99819 <62270592+min99819@users.noreply.github.com>
2023-07-03 21:19:52 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int dp[1001] = { 0 };
+	dp[1] = 0;
+	dp[2] = 1;
+	dp[3] = 0;
+	dp[4] = 1;
+	for (int i = 5; i <= n; i++) {
+		if (dp[i - 4] && dp[i - 3] && dp[i - 1]) {
+			dp[i] = 0;
+		}
+		else dp[i] = 1;
+	}
+	if (dp[n] == 1) cout << "SK";
+	else cout << "CY";
+}
