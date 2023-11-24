9a416f4215c6040d5aa9a88ccdbc03ec9df14f2d
min99819 <62270592+min99819@users.noreply.github.com>
2023-11-25 03:24:33 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int matrix1[101][101];
+    int matrix2[101][101];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> matrix2[i][j];
+        }
+    }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cout << matrix1[i][j] + matrix2[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
